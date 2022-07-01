d9e35a535f4f86e0d4c961725e2660f069b90050
chrispobrien <chris.obrien57@gmail.com>
2022-07-01 16:03:20 -0400

    Finished but for README.md

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreign_key: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreign_key: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
